b2b91be8beb5cbb66ae4593e25cafdb0bf9f2021
Shashwatdhiman <134190305+Shashwatdhiman@users.noreply.github.com>
2023-11-20 10:56:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Add your form submission logic here, e.g., sending data to a server
+    alert("Form submitted!");
+});
+
